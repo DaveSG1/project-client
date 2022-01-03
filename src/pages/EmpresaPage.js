759f8dbe0d9d01ffaf2dd22693a3234b574fd4a3
DavidSG <david_serna_83@hotmail.com>
2022-01-03 12:49:24 +0100

    seguimos dotando de contenido a la pagina Empresa y vamos haciendola responsive en su apartado roundedImages

--- src/pages/EmpresaPage.js
+++ src/pages/EmpresaPage.js
@@ -10,11 +10,27 @@ export default function EmpresaPage() {
 
             <div className="heading">
 
-                <img className="mainImage" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPDn8LZHXWSRKoJKw2hlh7L8SRfxUf_1leXA&usqp=CAU" alt="" />
+                <img className="mainImage" src="https://elegirhoy.com/uploads/fichas-eventos-imagenes/ruta-nocturna.jpg" alt="" />
                 <div className="text">
                     <h2 className="">Rutas exclusivas a caballo por toda España</h2>
                     <span className="">Somos una web dedicada al turismo activo. Nuestra principal actividad son las rutas a caballo por España, aptas para cualquier persona y edad, con el fin de ofrecer sensaciones únicas y momentos inolvidables a nuestros clientes.</span>
                 </div>
+
+                <div className="roundedImages">
+                    <div className="roundedBox">
+                        <img className="rounded" src="https://elegirhoy.com/uploads/fichas-eventos-imagenes/ruta-nocturna.jpg" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel ut esse perspiciatis perferendis recusandae, animi necessitatibus </p>
+                    </div>
+                    <div className="roundedBox">
+                        <img className="rounded" src="https://elegirhoy.com/uploads/fichas-eventos-imagenes/ruta-nocturna.jpg" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel ut esse perspiciatis perferendis recusandae, animi necessitatibus </p>
+                    </div>
+                    <div className="roundedBox">
+                        <img className="rounded" src="https://elegirhoy.com/uploads/fichas-eventos-imagenes/ruta-nocturna.jpg" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel ut esse perspiciatis perferendis recusandae, animi necessitatibus </p>
+                    </div>
+                </div>
+
                 <a className="buscarRutas" href="pages/RutasPage.js"> Buscar Rutas</a>
             </div>
 
