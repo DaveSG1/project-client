f135220dcc72715ed343debdb5d46b94538a0a51
DavidSG <david_serna_83@hotmail.com>
2022-01-07 13:50:15 +0100

    refactorizamos pagina inicio y pagina Empresa, asi como el css de ambas

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,9 +1,49 @@
-import React from "react"
+import React from 'react'
+import { useHistory } from 'react-router'
+/* import  from '../components/ ' */
 
-export default function Index() {
+export default function EmpresaPage() {
+
+
+    const history = useHistory();
+
+    function handleClick(e) {
+        history.push("/rutas")
+    }
+
+
     return (
         <div>
-            Contenido principal
+
+            <div className="heading">
+
+                <img className="mainImage" src="imagenes/fotoinicio.jpg" alt="" />
+                <div className="text">
+                    <h2 className="">Rutas exclusivas a caballo por toda España</h2>
+                    <span className="">Somos una web dedicada al turismo activo. Nuestra principal actividad son las rutas a caballo por España, aptas para cualquier persona y edad, con el fin de ofrecer sensaciones únicas y momentos inolvidables a nuestros clientes.</span>
+                </div>
+
+                <div className="roundedImages">
+                    <div className="roundedBox">
+                        <img className="rounded" src="https://perfectvenue.es/wp-content/uploads/2018/04/horse-riding-route-in-mallorca-2.jpg?v=162610863" alt="" />
+                        <p>Hemos seleccionado las mejores y más espectaculares rutas a caballo disponibles en España para que el cliente no se tenga que preocupar de nada, solamente de disfrutar de la experiencia.</p>
+                    </div>
+                    <div className="roundedBox">
+                        <img className="rounded" src="https://perfectvenue.es/wp-content/uploads/2018/04/horse-riding-route-in-mallorca.jpg?v=1626108637" alt="" />
+                        <p>Nuestros guías son profesionales nativos de cada región, con lo que, además de garantizar rutas 100% seguras, explican ampliamente los parajes por los que transcurre cada ruta.</p>
+                    </div>
+
+                    <div className="roundedBox">
+                        <img className="rounded" src="https://d2jhl5pzkfi24b.cloudfront.net/article_image/0001/02/1d5c3bd87cc5d063dbe8a69e86fb22681bd9db0b.jpeg" alt="./" />
+                        <p> El cuidado de los caballos es nuestra prioridad, por lo que, aparte de criarlos en entornos ideales, nos aseguramos de que en ningún momento realicen sobreesfuerzos y así disfrutemos todos de la experiencia, ya que sin ellos no sería posible.</p>
+                    </div>
+                </div>
+
+
+                <button onClick={(e) => handleClick(e)}>Buscar rutas</button>
+            </div>
+
+
         </div>
     )
 }
--- src/pages/EmpresaPage.js
+++ src/pages/EmpresaPage.js
@@ -1,47 +1,73 @@
 import React from 'react'
-import { useHistory } from 'react-router'
+/* import { useHistory } from 'react-router' */
 /* import  from '../components/ ' */
 
 export default function EmpresaPage() {
 
 
-    const history = useHistory();
-
-    function handleClick(e) {
-        history.push("/rutas")
-    }
+    /*  const history = useHistory();
+ 
+     function handleClick(e) {
+         history.push("/rutas")
+     } */
 
 
     return (
         <div>
 
-            <div className="heading">
+            <div className="headingEmpresa">
 
-                <img className="mainImage" src="imagenes/fotoinicio.jpg" alt="" />
+
                 <div className="text">
-                    <h2 className="">Rutas exclusivas a caballo por toda España</h2>
-                    <span className="">Somos una web dedicada al turismo activo. Nuestra principal actividad son las rutas a caballo por España, aptas para cualquier persona y edad, con el fin de ofrecer sensaciones únicas y momentos inolvidables a nuestros clientes.</span>
+                    <h2 className="">Sobre nosotros</h2>
+                    <span className="span">En los tiempos que corren, que el tiempo cada vez es más limitado, es un fastidio tener que estar buscando en distintas webs para disfrutar de una experiencia de ocio.
+                        Por ésto hemos seleccionado para nuestros clientes las mejores y más espectaculares rutas a caballo disponibles en España para que el cliente no se tenga que preocupar de nada, sólamene de disfrutar de la experiencia.
+                        De este modo evitamos el tedioso proceso de búsqueda entre cientos de web.
+                        Asimismo, coordinamos todo el proceso de reserva de la ruta elegida, únicamente contáctanos, del resto nos encargamos nosotros.</span>
                 </div>
 
-                <div className="roundedImages">
-                    <div className="roundedBox">
-                        <img className="rounded" src="https://perfectvenue.es/wp-content/uploads/2018/04/horse-riding-route-in-mallorca-2.jpg?v=162610863" alt="" />
-                        <p>Hemos seleccionado las mejores y más espectaculares rutas a caballo disponibles en España para que el cliente no se tenga que preocupar de nada, solamente de disfrutar de la experiencia.</p>
-                    </div>
-                    <div className="roundedBox">
-                        <img className="rounded" src="https://perfectvenue.es/wp-content/uploads/2018/04/horse-riding-route-in-mallorca.jpg?v=1626108637" alt="" />
-                        <p>Nuestros guías son profesionales nativos de cada región, con lo que, además de garantizar rutas 100% seguras, explican ampliamente los parajes por los que transcurre cada ruta.</p>
-                    </div>
+                <img className="mainImageEmpresa" src="imagenes/fotoinicio.jpg" alt="" />
 
-                    <div className="roundedBox">
-                        <img className="rounded" src="https://d2jhl5pzkfi24b.cloudfront.net/article_image/0001/02/1d5c3bd87cc5d063dbe8a69e86fb22681bd9db0b.jpeg" alt="./" />
-                        <p> El cuidado de los caballos es nuestra prioridad, por lo que, aparte de criarlos en entornos ideales, nos aseguramos de que en ningún momento realicen sobreesfuerzos y así disfrutemos todos de la experiencia, ya que sin ellos no sería posible.</p>
-                    </div>
-                </div>
-
-
-                <button onClick={(e) => handleClick(e)}>Buscar rutas</button>
             </div>
+
+
+
+            <div className="squaredImages">
+                <div className="squaredBox">
+                    <p>Proporcionamos todo todo el equipo necesario para disfrutar con comodidad y seguridad de la salida.</p>
+                    <img className="squared" src="https://perfectvenue.es/wp-content/uploads/2018/04/horse-riding-route-in-mallorca-2.jpg?v=162610863" alt="" />
+
+                </div>
+                <div className="squaredBox">
+                    <p>Grupos reducidos para ofrecer una atención personalizada, adaptándonos a vuestro nivel.</p>
+                    <img className="squared" src="https://perfectvenue.es/wp-content/uploads/2018/04/horse-riding-route-in-mallorca.jpg?v=1626108637" alt="" />
+
+                </div>
+
+                <div className="squaredBox">
+                    <p> Todas nuestras rutas están cubiertas por los seguros de Responsabilidad Civil y de Asistencia en Viaje según la normativa vigente.</p>
+                    <img className="squared" src="https://d2jhl5pzkfi24b.cloudfront.net/article_image/0001/02/1d5c3bd87cc5d063dbe8a69e86fb22681bd9db0b.jpeg" alt="./" />
+
+                </div>
+            </div>
+
+
+            <div className="moreInfoEmpresa">
+                <h2>Info adicional</h2>
+                <div>
+
+                    <p>Precios:
+                        • Por hora (20€)
+                        • ½ jornada (70€)
+                        • Dia completo (100€) (incluye picnic con bebida y almuerzo)</p>
+                    <p>Los precios por persona incluyen:
+                        • Caballos tranquilos y muy fiables.
+                        • Guia profesional.
+                        • Equipamiento completo.
+                        • Grupos reducidos para ofrecer una atención personalizada, adaptándonos a vuestro nivel.</p>
+                </div>
+            </div>
+
 
 
         </div>
