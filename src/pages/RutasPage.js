88106fe3f122b823745d7f8dd9d1bc6f4348f59c
DavidSG <david_serna_83@hotmail.com>
2022-02-24 13:35:20 +0100

    tras clase con miguel

--- src/pages/RutasPage.js
+++ src/pages/RutasPage.js
@@ -1,33 +1,26 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 
-
 import Ruta from "../components/Ruta";
 
 export default function RutasPage() {
   //   const { rutas, setRutas } = useContext(GlobalContext);
   const [rutas, setRutas] = useState([]);
 
+  // const { rutas, setRutas } = useContext(GlobalContext);
 
-    // const { rutas, setRutas } = useContext(GlobalContext);
-
-
-    let URL = "http://localhost:8000/api/rides";
+  let URL = "http://localhost:8000/api/rides";
 
   useEffect(() => {
     fetch(URL)
-      .then(r => r.json())
-      .then(data => setRutas(data));
-    }, [URL])
-
+      .then((r) => r.json())
+      .then((data) => setRutas(data));
+  }, [URL]);
 
   return (
     <div className="gridRutas">
-
-      {rutas.map(ruta => {       
-        return (
-          <Ruta ruta={ruta} key={ruta.id} />
-        );
+      {rutas.map((ruta) => {
+        return <Ruta ruta={ruta} key={ruta.id} />;
       })}
     </div>
   );
